8648a2cdc64da5e03c35d05cf9ab32cd4b336978
lifw555 <lifw555@0c61655d-0141-0410-b679-13ff037cbac1>
2009-11-26 11:33:50 +0000

    修正单元测试

--- trunk/java_version/fireflow/fireflow-engine/src/test/java/org/fireflow/engine/test/support/FireFlowAbstractTests.java
+++ trunk/java_version/fireflow/fireflow-engine/src/test/java/org/fireflow/engine/test/support/FireFlowAbstractTests.java
@@ -1,45 +1,51 @@
 package org.fireflow.engine.test.support;
 
+import org.fireflow.engine.RuntimeContext;
+import org.fireflow.engine.persistence.hibernate.FireWorkflowHelperDao;
 import org.junit.After;
 import org.junit.Before;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
+import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
+import org.springframework.transaction.support.TransactionTemplate;
 
 // hibernate
 @ContextConfiguration(locations = { "classpath:/config/applicationContext-hibernate.xml",
-									"classpath:/config/FireflowContext-hibernate.xml",
-									"classpath:/config/AllTheProcessHandlers.xml"
-								  })
+									"classpath:/config/FireflowContext-hibernate.xml", 
+									"classpath:/config/AllTheProcessHandlers.xml" })
 // jdbc
-//@ContextConfiguration(locations = { "classpath:/config/applicationContext-jdbc.xml",
-//									"classpath:/config/FireflowContext-jdbc.xml",
-//									"classpath:/config/AllTheProcessHandlers.xml"
-//								  })
-
+// @ContextConfiguration(locations = {"classpath:/config/applicationContext-jdbc.xml",
+// 									"classpath:/config/FireflowContext-jdbc.xml",
+// 									"classpath:/config/AllTheProcessHandlers.xml" })
 // JPA
-//@ContextConfiguration(locations = { "classpath:/config/applicationContext-jpa.xml",
-//									"classpath:/config/FireflowContext-jpa.xml",
-//									"classpath:/config/AllTheProcessHandlers.xml"
-//								  })
-public class FireFlowAbstractTests extends AbstractTransactionalJUnit4SpringContextTests
+// @ContextConfiguration(locations = {"classpath:/config/applicationContext-jpa.xml",
+// 									"classpath:/config/FireflowContext-jpa.xml",
+// 									"classpath:/config/AllTheProcessHandlers.xml" })
+public class FireFlowAbstractTests extends AbstractJUnit4SpringContextTests
 {
 	private static final Logger log = LoggerFactory.getLogger(FireFlowAbstractTests.class);
+
+	@Autowired
+	protected RuntimeContext runtimeContext = null;
+
+	@Autowired
+	protected TransactionTemplate transactionTemplate = null;
+
+	@Autowired
+	protected FireWorkflowHelperDao fireWorkflowHelperDao;
 
 	@Before
 	public void prepareTestData()
 	{
-		log.debug(this.getClass().getName());
-
-		// 如下两条是在需要执行每个方法前时初始化的数据脚本
-		// setSqlScriptEncoding("UTF-8");
-		// executeSqlScript("classpath:/sql/init.sql", false);
+		log.debug("方法执行前调用");
 	}
 
 	@After
 	public void destroyTestData()
 	{
-		// log.debug("destroyTestData");
+		// 每个测试方法执行完成后调用
+		log.debug("方法执行后调用");
 	}
 }
