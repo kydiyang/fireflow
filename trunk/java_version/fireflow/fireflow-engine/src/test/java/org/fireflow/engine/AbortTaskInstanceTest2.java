0d6eaec45e24f8d5a3e2eb5bb8adbd4236fa813e
lifw555 <lifw555@0c61655d-0141-0410-b679-13ff037cbac1>
2009-11-28 05:20:40 +0000

    这2个单元测试类通过测试。

--- trunk/java_version/fireflow/fireflow-engine/src/test/java/org/fireflow/engine/AbortTaskInstanceTest2.java
+++ trunk/java_version/fireflow/fireflow-engine/src/test/java/org/fireflow/engine/AbortTaskInstanceTest2.java
@@ -13,54 +13,23 @@ import org.fireflow.engine.taskinstance.AssignToCurrentUserAndCompleteWorkItemHa
 import org.fireflow.engine.taskinstance.DynamicAssignmentHandler;
 import org.fireflow.engine.test.support.FireFlowAbstractTests;
 import org.fireflow.kernel.KernelException;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.support.TransactionCallback;
 
 public class AbortTaskInstanceTest2 extends FireFlowAbstractTests {
-//    private final static String springConfigFile = "config/TestContext.xml";
-//    private static ClassPathResource resource = null;
-//    private static XmlBeanFactory beanFactory = null;
-//    private static TransactionTemplate transactionTemplate = null;
-//    private static RuntimeContext runtimeContext = null;
 
     //--------constant----------------------
     //客户电话，用于控制是否执行“发送手机短信通知客户收货”。通过设置mobile等于null和非null值分别进行测试。
 //    private final static String mobile = null;//"123123123123";
-
-    //-----variables-----------------
-    static IProcessInstance currentProcessInstance = null;
-    static String workItemAId = null;
-    static String workItemBId = null;
-    static String taskInstanceBId = null;
-    static String workItemCId = null;
- 
-    @BeforeClass
-    public static void setUpClass() throws Exception {
-//        resource = new ClassPathResource(springConfigFile);
-//        beanFactory = new XmlBeanFactory(resource);
-//        transactionTemplate = (TransactionTemplate) beanFactory.getBean("transactionTemplate");
-//        runtimeContext = (RuntimeContext) beanFactory.getBean("runtimeContext");
-//
-//        //首先将表中的数据清除
-//        transactionTemplate.execute(new TransactionCallbackWithoutResult() {
-//
-//            @Override
-//            protected void doInTransactionWithoutResult(TransactionStatus arg0) {
-//            	IFireWorkflowHelperDao helperDao = (IFireWorkflowHelperDao) beanFactory.getBean("FireWorkflowHelperDao");
-//                helperDao.clearAllTables();
-//            }
-//        });
-    }
-    
+	
     /**
      * 创建流程实例，并使之执行到B环节。
      */
     @Test
     public void testStartNewProcess() {
         System.out.println("--------------Start a new process ----------------");
-        currentProcessInstance = (IProcessInstance) transactionTemplate.execute(new TransactionCallback() {
+        IProcessInstance currentProcessInstance = (IProcessInstance) transactionTemplate.execute(new TransactionCallback() {
 
             public Object doInTransaction(TransactionStatus arg0) {
                 try {
@@ -90,7 +59,7 @@ public class AbortTaskInstanceTest2 extends FireFlowAbstractTests {
         assertNotNull(workItemList);
         assertEquals(1, workItemList.size());
         assertEquals(new Integer(IWorkItem.INITIALIZED), ((IWorkItem) workItemList.get(0)).getState());
-        workItemAId = ((IWorkItem) workItemList.get(0)).getId();
+        final String workItemAId = ((IWorkItem) workItemList.get(0)).getId();
         
         //结束Activity A的工作项，使流程流转到B环节
         transactionTemplate.execute(new TransactionCallback() {
@@ -114,13 +83,13 @@ public class AbortTaskInstanceTest2 extends FireFlowAbstractTests {
         taskInstanceList = persistenceService.findTaskInstancesForProcessInstance(currentProcessInstance.getId(), "AbortTaskInstance2.B");
         assertNotNull(taskInstanceList);
         assertEquals(1, taskInstanceList.size());    
-        taskInstanceBId = ((ITaskInstance)taskInstanceList.get(0)).getId();
+        //String taskInstanceBId = ((ITaskInstance)taskInstanceList.get(0)).getId();
         
         workItemList = persistenceService.findTodoWorkItems(AssignToCurrentUserAndCompleteWorkItemHandler.ACTOR_ID, "AbortTaskInstance2", "AbortTaskInstance2.B.TaskB");
         assertNotNull(workItemList);
         assertEquals(1, workItemList.size());
         assertEquals(new Integer(IWorkItem.INITIALIZED), ((IWorkItem) workItemList.get(0)).getState());
-        workItemBId = ((IWorkItem) workItemList.get(0)).getId();        
+        //String workItemBId = ((IWorkItem) workItemList.get(0)).getId();        
     }    
     
     /**
@@ -128,6 +97,69 @@ public class AbortTaskInstanceTest2 extends FireFlowAbstractTests {
      */
     @Test
     public void testAbortActivityB() {
+    	
+    	IProcessInstance currentProcessInstance = (IProcessInstance) transactionTemplate.execute(new TransactionCallback() {
+
+            public Object doInTransaction(TransactionStatus arg0) {
+                try {
+                    IWorkflowSession workflowSession = runtimeContext.getWorkflowSession();
+                    //启动"/workflowdefinition/Jump.xml
+                    IProcessInstance processInstance = workflowSession.createProcessInstance("AbortTaskInstance2",AssignToCurrentUserAndCompleteWorkItemHandler.ACTOR_ID);
+
+                    processInstance.run();
+                    return processInstance;
+                } catch (EngineException ex) {
+                    Logger.getLogger(FireWorkflowEngineTest.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (KernelException ex) {
+                    Logger.getLogger(FireWorkflowEngineTest.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                return null;
+            }
+        });
+        assertNotNull(currentProcessInstance);
+
+        IPersistenceService persistenceService = runtimeContext.getPersistenceService();
+        List<ITaskInstance> taskInstanceList = persistenceService.findTaskInstancesForProcessInstance(currentProcessInstance.getId(), "AbortTaskInstance2.A");
+        assertNotNull(taskInstanceList);
+        assertEquals(1, taskInstanceList.size());
+        
+
+        List<IWorkItem> workItemList = persistenceService.findTodoWorkItems(AssignToCurrentUserAndCompleteWorkItemHandler.ACTOR_ID, "AbortTaskInstance2", "AbortTaskInstance2.A.TaskA");
+        assertNotNull(workItemList);
+        assertEquals(1, workItemList.size());
+        assertEquals(new Integer(IWorkItem.INITIALIZED), ((IWorkItem) workItemList.get(0)).getState());
+        final String workItemAId = ((IWorkItem) workItemList.get(0)).getId();
+        
+        //结束Activity A的工作项，使流程流转到B环节
+        transactionTemplate.execute(new TransactionCallback() {
+
+            public Object doInTransaction(TransactionStatus arg0) {
+                try {
+                    IWorkflowSession workflowSession = runtimeContext.getWorkflowSession();
+                    IWorkItem wi = workflowSession.findWorkItemById(workItemAId);
+                    wi.claim();
+                    wi.complete();
+                    return null;
+                } catch (EngineException ex) {
+                    Logger.getLogger(FireWorkflowEngineTest.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (KernelException ex) {
+                    Logger.getLogger(FireWorkflowEngineTest.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                return null;
+            }
+        });
+        
+        taskInstanceList = persistenceService.findTaskInstancesForProcessInstance(currentProcessInstance.getId(), "AbortTaskInstance2.B");
+        assertNotNull(taskInstanceList);
+        assertEquals(1, taskInstanceList.size());    
+        final String taskInstanceBId = ((ITaskInstance)taskInstanceList.get(0)).getId();
+        
+        workItemList = persistenceService.findTodoWorkItems(AssignToCurrentUserAndCompleteWorkItemHandler.ACTOR_ID, "AbortTaskInstance2", "AbortTaskInstance2.B.TaskB");
+        assertNotNull(workItemList);
+        assertEquals(1, workItemList.size());
+        assertEquals(new Integer(IWorkItem.INITIALIZED), ((IWorkItem) workItemList.get(0)).getState());
+        //String workItemBId = ((IWorkItem) workItemList.get(0)).getId();        
+    	
         transactionTemplate.execute(new TransactionCallback() {
 
             public Object doInTransaction(TransactionStatus arg0) {
@@ -148,9 +180,10 @@ public class AbortTaskInstanceTest2 extends FireFlowAbstractTests {
                 return null;
             }
         });    	
-        IPersistenceService persistenceService = runtimeContext.getPersistenceService();
+        //IPersistenceService persistenceService = runtimeContext.getPersistenceService();
         
-        List<ITaskInstance> taskInstanceList = persistenceService.findTaskInstancesForProcessInstance(currentProcessInstance.getId(), "AbortTaskInstance2.B");
+       // List<ITaskInstance> 
+        taskInstanceList = persistenceService.findTaskInstancesForProcessInstance(currentProcessInstance.getId(), "AbortTaskInstance2.B");
         assertNotNull(taskInstanceList);
         assertEquals(1, taskInstanceList.size());
         int taskInstanceState = ((ITaskInstance)taskInstanceList.get(0)).getState();
@@ -161,17 +194,13 @@ public class AbortTaskInstanceTest2 extends FireFlowAbstractTests {
         assertEquals(1, taskInstanceList.size());
         taskInstanceState = ((ITaskInstance)taskInstanceList.get(0)).getState();
         assertEquals(ITaskInstance.RUNNING,taskInstanceState);
-        
-     
-        
-        List<IWorkItem> workItemList = persistenceService.findTodoWorkItems("zhangsan", "AbortTaskInstance2", "AbortTaskInstance2.D.TaskD");
+      
+        //List<IWorkItem> 
+        workItemList = persistenceService.findTodoWorkItems("zhangsan", "AbortTaskInstance2", "AbortTaskInstance2.D.TaskD");
         assertNotNull(workItemList);
         assertEquals(1, workItemList.size());
         assertEquals(new Integer(IWorkItem.RUNNING), ((IWorkItem) workItemList.get(0)).getState());
-        workItemCId = ((IWorkItem) workItemList.get(0)).getId();          
+        //workItemCId = ((IWorkItem) workItemList.get(0)).getId();          
     }
-    @Test
-    public void clear(){
-    	fireWorkflowHelperDao.clearAllTables();
-    }
+
 }
