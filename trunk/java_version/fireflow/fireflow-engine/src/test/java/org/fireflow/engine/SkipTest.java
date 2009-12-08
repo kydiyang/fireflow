b882f2b46ef9d2472062b3c2e771f869e4c39b1a
lifw555 <lifw555@0c61655d-0141-0410-b679-13ff037cbac1>
2009-12-08 05:39:42 +0000

    这个单元测试类通过测试。

--- trunk/java_version/fireflow/fireflow-engine/src/test/java/org/fireflow/engine/SkipTest.java
+++ trunk/java_version/fireflow/fireflow-engine/src/test/java/org/fireflow/engine/SkipTest.java
@@ -19,11 +19,6 @@ import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.support.TransactionCallback;
 
 public class SkipTest extends FireFlowAbstractTests {
-//    private final static String springConfigFile = "config/TestContext.xml";
-//    private static ClassPathResource resource = null;
-//    private static XmlBeanFactory beanFactory = null;
-//    private static TransactionTemplate transactionTemplate = null;
-//    private static RuntimeContext runtimeContext = null;
 
         //-----variables-----------------
     static IProcessInstance currentProcessInstance = null;
@@ -31,24 +26,6 @@ public class SkipTest extends FireFlowAbstractTests {
     static String workItem2Id = null;
     static String workItem3Id = null;
     static String workItem5Id = null;
-    
-//    @BeforeClass
-//    public static void setUpClass() throws Exception {
-//        resource = new ClassPathResource(springConfigFile);
-//        beanFactory = new XmlBeanFactory(resource);
-//        transactionTemplate = (TransactionTemplate) beanFactory.getBean("transactionTemplate");
-//        runtimeContext = (RuntimeContext) beanFactory.getBean("runtimeContext");
-//
-//        //首先将表中的数据清除
-//        transactionTemplate.execute(new TransactionCallbackWithoutResult() {
-//
-//            @Override
-//            protected void doInTransactionWithoutResult(TransactionStatus arg0) {
-//            	IFireWorkflowHelperDao helperDao = (IFireWorkflowHelperDao) beanFactory.getBean("FireWorkflowHelperDao");
-//                helperDao.clearAllTables();
-//            }
-//        });
-//    }
 
     /**
      * 创建流程实例，并执行实例的run方法。
@@ -77,19 +54,19 @@ public class SkipTest extends FireFlowAbstractTests {
         assertNotNull(currentProcessInstance);
 
         IPersistenceService persistenceService = runtimeContext.getPersistenceService();
-        List taskInstanceList = persistenceService.findTaskInstancesForProcessInstance(currentProcessInstance.getId(), "skip2.Activity1");
+        List<ITaskInstance> taskInstanceList = persistenceService.findTaskInstancesForProcessInstance(currentProcessInstance.getId(), "skip2.Activity1");
         assertNotNull(taskInstanceList);
         assertEquals(1, taskInstanceList.size());
         ITaskInstance taskInst = ((ITaskInstance) taskInstanceList.get(0));
         assertEquals(new Integer(ITaskInstance.RUNNING),taskInst .getState());
         assertEquals(1,taskInst.getStepNumber().intValue());
 
-        List workItemList = persistenceService.findTodoWorkItems(CurrentUserAssignmentHandlerMock.ACTOR_ID, "skip2", "skip2.Activity1.Task1");
+        List<IWorkItem> workItemList = persistenceService.findTodoWorkItems(CurrentUserAssignmentHandlerMock.ACTOR_ID, "skip2", "skip2.Activity1.Task1");
         assertNotNull(workItemList);
         assertEquals(1, workItemList.size());
         assertEquals(new Integer(IWorkItem.RUNNING), ((IWorkItem) workItemList.get(0)).getState());
 
-        List tokensList = persistenceService.findTokensForProcessInstance(currentProcessInstance.getId(), null);
+        List<IToken> tokensList = persistenceService.findTokensForProcessInstance(currentProcessInstance.getId(), null);
         assertNotNull(tokensList);
         assertEquals(1, tokensList.size());
         IToken token = (IToken)tokensList.get(0);
@@ -100,6 +77,48 @@ public class SkipTest extends FireFlowAbstractTests {
     
     @Test
     public void testCompleteWorkItem1(){
+    	
+    	currentProcessInstance = (IProcessInstance) transactionTemplate.execute(new TransactionCallback() {
+
+            public Object doInTransaction(TransactionStatus arg0) {
+                try {
+                    IWorkflowSession workflowSession = runtimeContext.getWorkflowSession();
+                    //启动"/workflowdefinition/Loop.xml
+                    IProcessInstance processInstance = workflowSession.createProcessInstance("skip2",AssignmentHandlerMock.ACTOR_ID);
+                    processInstance.setProcessInstanceVariable("flag", "skip");
+                    processInstance.run();
+                    return processInstance;
+                } catch (EngineException ex) {
+                    Logger.getLogger(FireWorkflowEngineTest.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (KernelException ex) {
+                    Logger.getLogger(FireWorkflowEngineTest.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                return null;
+            }
+        });
+        assertNotNull(currentProcessInstance);
+
+        IPersistenceService persistenceService = runtimeContext.getPersistenceService();
+        List<ITaskInstance> taskInstanceList = persistenceService.findTaskInstancesForProcessInstance(currentProcessInstance.getId(), "skip2.Activity1");
+        assertNotNull(taskInstanceList);
+        assertEquals(1, taskInstanceList.size());
+        ITaskInstance taskInst = ((ITaskInstance) taskInstanceList.get(0));
+        assertEquals(new Integer(ITaskInstance.RUNNING),taskInst .getState());
+        assertEquals(1,taskInst.getStepNumber().intValue());
+
+        List<IWorkItem> workItemList = persistenceService.findTodoWorkItems(CurrentUserAssignmentHandlerMock.ACTOR_ID, "skip2", "skip2.Activity1.Task1");
+        assertNotNull(workItemList);
+        assertEquals(1, workItemList.size());
+        assertEquals(new Integer(IWorkItem.RUNNING), ((IWorkItem) workItemList.get(0)).getState());
+
+        List<IToken> tokensList = persistenceService.findTokensForProcessInstance(currentProcessInstance.getId(), null);
+        assertNotNull(tokensList);
+        assertEquals(1, tokensList.size());
+        IToken token = (IToken)tokensList.get(0);
+        assertEquals(1,token.getStepNumber().intValue());
+
+        workItem1Id = ((IWorkItem) workItemList.get(0)).getId();
+    	//-------------------------------------------------------
         transactionTemplate.execute(new TransactionCallback() {
 
             public Object doInTransaction(TransactionStatus arg0) {
@@ -117,9 +136,8 @@ public class SkipTest extends FireFlowAbstractTests {
             }
         });
         
-        IPersistenceService persistenceService = runtimeContext.getPersistenceService();
         
-        List taskInstanceList = persistenceService.findTaskInstancesForProcessInstance(currentProcessInstance.getId(), "skip2.Activity2");
+        taskInstanceList = persistenceService.findTaskInstancesForProcessInstance(currentProcessInstance.getId(), "skip2.Activity2");
         if (taskInstanceList==null){taskInstanceList = new ArrayList();}
         assertEquals(0,taskInstanceList.size());
         
@@ -130,23 +148,20 @@ public class SkipTest extends FireFlowAbstractTests {
         taskInstanceList = persistenceService.findTaskInstancesForProcessInstance(currentProcessInstance.getId(), "skip2.Activity3");
         assertNotNull(taskInstanceList);
         assertEquals(1, taskInstanceList.size());
-        ITaskInstance taskInst = ((ITaskInstance) taskInstanceList.get(0));
+        taskInst = ((ITaskInstance) taskInstanceList.get(0));
         assertEquals(new Integer(ITaskInstance.RUNNING),taskInst .getState());
         assertEquals(3,taskInst.getStepNumber().intValue());
 
-        List workItemList = persistenceService.findTodoWorkItems(CurrentUserAssignmentHandlerMock.ACTOR_ID, "skip2", "skip2.Activity3.Task3");
+        workItemList = persistenceService.findTodoWorkItems(CurrentUserAssignmentHandlerMock.ACTOR_ID, "skip2", "skip2.Activity3.Task3");
         assertNotNull(workItemList);
         assertEquals(1, workItemList.size());
         assertEquals(new Integer(IWorkItem.INITIALIZED), ((IWorkItem) workItemList.get(0)).getState());
 
-        List tokensList = persistenceService.findTokensForProcessInstance(currentProcessInstance.getId(), null);
+        tokensList = persistenceService.findTokensForProcessInstance(currentProcessInstance.getId(), null);
         assertNotNull(tokensList);
         assertEquals(1, tokensList.size());
-        IToken token = (IToken)tokensList.get(0);
+        token = (IToken)tokensList.get(0);
         assertEquals(3,token.getStepNumber().intValue());        
     }
-    @Test
-    public void clear(){
-    	fireWorkflowHelperDao.clearAllTables();
-    }
+
 }
