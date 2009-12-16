2975586fd5f64b9df21fd22b56e5ec415562de3b
mingjie.mj <mingjie.mj@0c61655d-0141-0410-b679-13ff037cbac1>
2009-12-16 10:26:17 +0000

    格式

--- trunk/java_version/fireflow/fireflow-engine/src/main/java/org/fireflow/engine/impl/ProcessInstanceVar.java
+++ trunk/java_version/fireflow/fireflow-engine/src/main/java/org/fireflow/engine/impl/ProcessInstanceVar.java
@@ -25,96 +25,86 @@ import java.util.Date;
  * @author 非也,nychen2000@163.com
  */
 public class ProcessInstanceVar {
-    ProcessInstanceVarPk varPrimaryKey = null;
-    String value = null;
-    String valueType = null;
+	ProcessInstanceVarPk varPrimaryKey = null;
+	String value = null;
+	String valueType = null;
 
-    public ProcessInstanceVarPk getVarPrimaryKey() {
-        return varPrimaryKey;
-    }
+	public ProcessInstanceVarPk getVarPrimaryKey() {
+		return varPrimaryKey;
+	}
 
-    public void setVarPrimaryKey(ProcessInstanceVarPk varPrimaryKey) {
-        this.varPrimaryKey = varPrimaryKey;
-    }
+	public void setVarPrimaryKey(ProcessInstanceVarPk varPrimaryKey) {
+		this.varPrimaryKey = varPrimaryKey;
+	}
 
-    public String getName(){
-    	return varPrimaryKey==null?null:varPrimaryKey.getName();
-    }
-    
-    public String getProcessInstanceId(){
-    	return varPrimaryKey==null?null:varPrimaryKey.getProcessInstanceId();
-    }
+	public String getName() {
+		return varPrimaryKey == null ? null : varPrimaryKey.getName();
+	}
 
-    public Object getValue() {
-    	if(Integer.class.getName().equals(this.valueType) || int.class.getName().equals(this.valueType))
-    	{ 
-    		return Integer.parseInt(this.value);
-    	}
-    	else if(Long.class.getName().equals(this.valueType) || long.class.getName().equals(this.valueType))
-    	{
-    		return Long.parseLong(this.value);
-    	}
-    	else if(String.class.getName().equals(this.valueType))
-    	{
-    		return this.value;
-    	}
-    	else if(Float.class.getName().equals(this.valueType) || float.class.getName().equals(this.valueType))
-    	{
-    		return Float.parseFloat(this.value);
-    	}
-    	else if(Double.class.getName().equals(this.valueType) || double.class.getName().equals(this.valueType))
-    	{
-    		return Double.parseDouble(this.value);
-    	}
-    	else if(Boolean.class.getName().equals(this.valueType) || boolean.class.getName().equals(this.valueType))
-    	{
-    		return Boolean.parseBoolean(this.value);
-    	}
-    	else if(Date.class.getName().equals(this.valueType))
-    	{
-    		SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMdd HHmmss");
-			try
-			{
+	public String getProcessInstanceId() {
+		return varPrimaryKey == null ? null : varPrimaryKey
+				.getProcessInstanceId();
+	}
+
+	public Object getValue() {
+		if (Integer.class.getName().equals(this.valueType)
+				|| int.class.getName().equals(this.valueType)) {
+			return Integer.parseInt(this.value);
+		} else if (Long.class.getName().equals(this.valueType)
+				|| long.class.getName().equals(this.valueType)) {
+			return Long.parseLong(this.value);
+		} else if (String.class.getName().equals(this.valueType)) {
+			return this.value;
+		} else if (Float.class.getName().equals(this.valueType)
+				|| float.class.getName().equals(this.valueType)) {
+			return Float.parseFloat(this.value);
+		} else if (Double.class.getName().equals(this.valueType)
+				|| double.class.getName().equals(this.valueType)) {
+			return Double.parseDouble(this.value);
+		} else if (Boolean.class.getName().equals(this.valueType)
+				|| boolean.class.getName().equals(this.valueType)) {
+			return Boolean.parseBoolean(this.value);
+		} else if (Date.class.getName().equals(this.valueType)) {
+			SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMdd HHmmss");
+			try {
 				return formatter.parse(this.value);
-			}
-			catch (ParseException e)
-			{
+			} catch (ParseException e) {
 				return null;
 			}
-    	}
-        return value;
-    }
+		}
+		return value;
+	}
 
-    public void setValue(Object v) {
-        this.value = String.valueOf(v);
-    }
-    
-    public String getValueType()
-	{
+	public void setValue(Object v) {
+		this.value = String.valueOf(v);
+	}
+
+	public String getValueType() {
 		return valueType;
 	}
 
-	public void setValueType(String valueType)
-	{
+	public void setValueType(String valueType) {
 		this.valueType = valueType;
 	}
 
-    public boolean equals(Object obj){
-        if (this==obj) return true;
-        if (!(obj instanceof ProcessInstanceVar))return false;
-        ProcessInstanceVar var = (ProcessInstanceVar)obj;
-        if (var.getVarPrimaryKey().equals(this.getVarPrimaryKey())){
-            return true;
-        }else{
-            return false;
-        }
-    }
-    
-    public int hashCode(){
-        if (this.getVarPrimaryKey()==null){
-            return super.hashCode();
-        }else{
-            return this.getVarPrimaryKey().hashCode();
-        }
-    }
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!(obj instanceof ProcessInstanceVar))
+			return false;
+		ProcessInstanceVar var = (ProcessInstanceVar) obj;
+		if (var.getVarPrimaryKey().equals(this.getVarPrimaryKey())) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public int hashCode() {
+		if (this.getVarPrimaryKey() == null) {
+			return super.hashCode();
+		} else {
+			return this.getVarPrimaryKey().hashCode();
+		}
+	}
 }
