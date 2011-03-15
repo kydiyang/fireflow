7139152354653007be5169d06b864968b875f5f4
westerly.lzh <westerly.lzh@0c61655d-0141-0410-b679-13ff037cbac1>
2011-03-15 13:09:39 +0000

    这里把tasksElement修改为servicesTask有利于强化fireFlow面向服务的概念，但是这里的parent应当 就是services元素了，怎么又通过qname又给parent添加了个services子元素？ --westerly

--- java_version_20/fireflow-model/src/main/java/org/fireflow/model/io/Dom4JServiceSerializer.java
+++ java_version_20/fireflow-model/src/main/java/org/fireflow/model/io/Dom4JServiceSerializer.java
@@ -44,10 +44,8 @@ import org.fireflow.model.servicedef.ServicePropGroup;
 public class Dom4JServiceSerializer implements ModelElementNames {
 	private static DocumentFactory df = new DocumentFactory();
 
-	public void serialize(List<Service> services, OutputStream out)
-			throws IOException, SerializerException {
-		Element servicesElement = df.createElement(new QName(SERVICES,
-				SERVICE_NS));
+	public void serialize(List<Service> services, OutputStream out) throws IOException, SerializerException {
+		Element servicesElement = df.createElement(new QName(SERVICES, SERVICE_NS));
 
 		servicesElement.addNamespace(SERVICE_NS_PREFIX, SERVICE_NS_URI);
 		servicesElement.addNamespace(XSD_NS_PREFIX, XSD_URI);
@@ -57,7 +55,7 @@ public class Dom4JServiceSerializer implements ModelElementNames {
 		servicesElement.addAttribute(qname, SERVICE_SCHEMA_LOCATION);
 
 		this.writeServices(services, servicesElement);
-		
+
 		Document document = df.createDocument(servicesElement);
 
 		// write the document to the output stream
@@ -70,53 +68,48 @@ public class Dom4JServiceSerializer implements ModelElementNames {
 		out.flush();
 	}
 
-	public void writeServices(List<Service> services, Element parent)
-			throws SerializerException {
+	public void writeServices(List<Service> services, Element parent) throws SerializerException {
 		if (services == null || services.size() == 0) {
 			return;
 		}
-		QName qname = df.createQName(SERVICES, SERVICE_NS_PREFIX,
-				SERVICE_NS_URI);
-		Element tasksElement = Util4Serializer.addElement(parent, qname);
+		QName qname = df.createQName(SERVICES, SERVICE_NS_PREFIX, SERVICE_NS_URI);
+		// 这里把tasksElement修改为servicesTask有利于强化fireFlow面向服务的概念，但是这里的parent应当
+		// 就是services元素了，怎么又通过qname又给parent添加了个services子元素？ --westerly
+		// Element tasksElement = Util4Serializer.addElement(parent, qname);
+		Element servicesElement = Util4Serializer.addElement(parent, qname);
 		Iterator<Service> iter = services.iterator();
 
 		while (iter.hasNext()) {
-			writeService((Service) iter.next(), tasksElement);
+			// writeService((Service) iter.next(), tasksElement);
+			writeService((Service) iter.next(), servicesElement);
 		}
 	}
 
-	protected void writeService(Service task, Element parent)
-			throws SerializerException {
-		QName qname = df
-				.createQName(SERVICE, SERVICE_NS_PREFIX, SERVICE_NS_URI);
-		Element taskElement = Util4Serializer.addElement(parent, qname);
+	protected void writeService(Service service, Element parent) throws SerializerException {
+		QName qname = df.createQName(SERVICE, SERVICE_NS_PREFIX, SERVICE_NS_URI);
+		Element serviceElement = Util4Serializer.addElement(parent, qname);
 
-		taskElement.addAttribute(ID, task.getId());
-		taskElement.addAttribute(NAME, task.getName());
-		taskElement.addAttribute(SERVICE_TYPE, task.getServiceType());
-		if (task.getDisplayName() != null
-				&& !task.getDisplayName().trim().equals("")) {
-			taskElement.addAttribute(DISPLAY_NAME, task.getDisplayName());
+		serviceElement.addAttribute(ID, service.getId());
+		serviceElement.addAttribute(NAME, service.getName());
+		serviceElement.addAttribute(SERVICE_TYPE, service.getServiceType());
+		if (service.getDisplayName() != null && !service.getDisplayName().trim().equals("")) {
+			serviceElement.addAttribute(DISPLAY_NAME, service.getDisplayName());
 		}
 
-		if (task.getBizCategory() != null
-				&& !task.getBizCategory().trim().equals("")) {
-			taskElement.addAttribute(BIZ_CATEGORY, task.getBizCategory());
+		if (service.getBizCategory() != null && !service.getBizCategory().trim().equals("")) {
+			serviceElement.addAttribute(BIZ_CATEGORY, service.getBizCategory());
 		}
 
-		if (task.getExecutorName() != null
-				&& !task.getExecutorName().trim().equals("")) {
-			taskElement.addAttribute(EXECUTOR_NAME, task.getExecutorName());
+		if (service.getExecutorName() != null && !service.getExecutorName().trim().equals("")) {
+			serviceElement.addAttribute(EXECUTOR_NAME, service.getExecutorName());
 		}
 
-		if (task.getDescription() != null
-				&& !task.getDescription().trim().equals("")) {
-			Util4Serializer.addElement(taskElement, DESCRIPTION, task
-					.getDescription());
+		if (service.getDescription() != null && !service.getDescription().trim().equals("")) {
+			Util4Serializer.addElement(serviceElement, DESCRIPTION, service.getDescription());
 		}
 
-		writeOperations(task.getOperations(), taskElement);
-		writeServicePropGroups(task.getServicePropGroups(), taskElement);
+		writeOperations(service.getOperations(), serviceElement);
+		writeServicePropGroups(service.getServicePropGroups(), serviceElement);
 	}
 
 	protected void writeOperations(List<Operation> operations, Element parent) {
@@ -126,62 +119,44 @@ public class Dom4JServiceSerializer implements ModelElementNames {
 		Element operationsElem = Util4Serializer.addElement(parent, OPERATIONS);
 
 		for (Operation operation : operations) {
-			Element operationElem = Util4Serializer.addElement(operationsElem,
-					OPERATION);
-			Util4Serializer.addElement(operationElem, OPERATION_NAME, operation
-					.getOperationName());
+			Element operationElem = Util4Serializer.addElement(operationsElem, OPERATION);
+			Util4Serializer.addElement(operationElem, OPERATION_NAME, operation.getOperationName());
 			IOSpecification iospec = operation.getIOSpecification();
 			if (iospec == null) {
 				continue;
 			}
 			if ((iospec.getInputs() != null && iospec.getInputs().size() > 0)
-					|| (iospec.getOutputs() != null && iospec.getOutputs()
-							.size() > 0)) {
-				Element iospecElem = Util4Serializer.addElement(operationElem,
-						IO_SPECIFICATION);
+					|| (iospec.getOutputs() != null && iospec.getOutputs().size() > 0)) {
+				Element iospecElem = Util4Serializer.addElement(operationElem, IO_SPECIFICATION);
 				if (iospec.getInputs() != null && iospec.getInputs().size() > 0) {
-					Element inputsElem = Util4Serializer.addElement(iospecElem,
-							INPUTS);
+					Element inputsElem = Util4Serializer.addElement(iospecElem, INPUTS);
 					List<Input> inputs = iospec.getInputs();
 					for (Input input : inputs) {
-						Element inputElem = Util4Serializer.addElement(
-								inputsElem, INPUT);
+						Element inputElem = Util4Serializer.addElement(inputsElem, INPUT);
 						inputElem.addAttribute(NAME, input.getName());
-						if (input.getDisplayName() != null
-								&& !input.getDisplayName().trim().equals("")) {
-							inputElem.addAttribute(DISPLAY_NAME, input
-									.getDisplayName());
+						if (input.getDisplayName() != null && !input.getDisplayName().trim().equals("")) {
+							inputElem.addAttribute(DISPLAY_NAME, input.getDisplayName());
 						}
 						inputElem.addAttribute(DATA_TYPE, input.getDataType());
-						if (input.getDefaultValueAsString() != null
-								&& !input.getDefaultValueAsString().equals("")) {
-							inputElem.addAttribute(DEFAULT_VALUE, input
-									.getDefaultValueAsString());
+						if (input.getDefaultValueAsString() != null && !input.getDefaultValueAsString().equals("")) {
+							inputElem.addAttribute(DEFAULT_VALUE, input.getDefaultValueAsString());
 						}
 
-						if (input.getDataPattern() != null
-								&& !input.getDataPattern().trim().equals("")) {
-							inputElem.addAttribute(DATA_PATTERN, input
-									.getDataPattern());
+						if (input.getDataPattern() != null && !input.getDataPattern().trim().equals("")) {
+							inputElem.addAttribute(DATA_PATTERN, input.getDataPattern());
 						}
 					}
 				}
-				if (iospec.getOutputs() != null
-						&& iospec.getOutputs().size() > 0) {
-					Element outputsElem = Util4Serializer.addElement(
-							iospecElem, OUTPUTS);
+				if (iospec.getOutputs() != null && iospec.getOutputs().size() > 0) {
+					Element outputsElem = Util4Serializer.addElement(iospecElem, OUTPUTS);
 					List<Output> outputs = iospec.getOutputs();
 					for (Output output : outputs) {
-						Element outputElem = Util4Serializer.addElement(
-								outputsElem, OUTPUT);
+						Element outputElem = Util4Serializer.addElement(outputsElem, OUTPUT);
 						outputElem.addAttribute(NAME, output.getName());
-						if (output.getDisplayName() != null
-								&& !output.getDisplayName().trim().equals("")) {
-							outputElem.addAttribute(DISPLAY_NAME, output
-									.getDisplayName());
+						if (output.getDisplayName() != null && !output.getDisplayName().trim().equals("")) {
+							outputElem.addAttribute(DISPLAY_NAME, output.getDisplayName());
 						}
-						outputElem
-								.addAttribute(DATA_TYPE, output.getDataType());
+						outputElem.addAttribute(DATA_TYPE, output.getDataType());
 					}
 				}
 			}
@@ -189,31 +164,24 @@ public class Dom4JServiceSerializer implements ModelElementNames {
 
 	}
 
-	protected void writeServicePropGroups(List<ServicePropGroup> propGroups,
-			Element parent) {
+	protected void writeServicePropGroups(List<ServicePropGroup> propGroups, Element parent) {
 		if (propGroups == null || propGroups.size() == 0) {
 			return;
 		}
-		Element propGroupsElem = Util4Serializer
-				.addElement(parent, PROP_GROUPS);
+		Element propGroupsElem = Util4Serializer.addElement(parent, PROP_GROUPS);
 		for (ServicePropGroup propGroup : propGroups) {
-			Element propGroupElem = Util4Serializer.addElement(propGroupsElem,
-					PROP_GROUP);
+			Element propGroupElem = Util4Serializer.addElement(propGroupsElem, PROP_GROUP);
 			propGroupElem.addAttribute(NAME, propGroup.getName());
-			propGroupElem
-					.addAttribute(DISPLAY_NAME, propGroup.getDisplayName());
+			propGroupElem.addAttribute(DISPLAY_NAME, propGroup.getDisplayName());
 
 			List<ServiceProp> propList = propGroup.getServiceProps();
 			if (propList != null) {
 				for (ServiceProp serviceProp : propList) {
-					Element propElem = Util4Serializer.addElement(
-							propGroupElem, PROP);
+					Element propElem = Util4Serializer.addElement(propGroupElem, PROP);
 					propElem.addAttribute(NAME, serviceProp.getName());
-					propElem.addAttribute(DISPLAY_NAME, serviceProp
-							.getDisplayName());
+					propElem.addAttribute(DISPLAY_NAME, serviceProp.getDisplayName());
 					propElem.addAttribute(VALUE, serviceProp.getValue());
-					Util4Serializer.addElement(propElem, DESCRIPTION,
-							serviceProp.getDescription());
+					Util4Serializer.addElement(propElem, DESCRIPTION, serviceProp.getDescription());
 				}
 			}
 		}
